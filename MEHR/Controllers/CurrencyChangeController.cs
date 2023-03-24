61b3c87c36a763d1603b82643de572ee25c0f386
Nico-Serge <nico.zimmermann@aol.de>
2023-03-24 12:15:11 +0100

    Ein bisschen Api zeugs blahh

---
+++ MEHR/Controllers/CurrencyChangeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace MEHR.Controllers
+{
+    [Controller]
+    [Route("/CurrencyApi")]
+    public class CurrencyChangeController : Controller
+    {
+        public class CurrencyResult
+        {
+            public Dictionary<string, float> data { get; set; } = new();
+
+        }
+
+        [HttpGet]
+        [Route("/ChangeToEuro")]
+        public decimal changeToEuro(decimal value, string currency)
+        {
+            var client = new RestClient("https://api.freecurrencyapi.com/v1/latest");
+            var request = new RestRequest();
+            request.AddParameter("apikey", "vfhrYYrcsUrh9kFEWuYXmDUmMzo0H5mvwaAvVeDz");
+            var response = client.Get<CurrencyResult>(request)!;
+            return (value / (decimal)response.data[currency]) * (decimal)response.data["EUR"];
+
+        }
+
+        [HttpGet]
+        [Route("/ChangeGeneral")]
+        public decimal changeCurrency(decimal value, string sourceCurrency, string targetCurrency)
+        {
+            var client = new RestClient("https://api.freecurrencyapi.com/v1/latest");
+            var request = new RestRequest();
+            request.AddParameter("apikey", "vfhrYYrcsUrh9kFEWuYXmDUmMzo0H5mvwaAvVeDz");
+            var response = client.Get<CurrencyResult>(request)!;
+            return (value / (decimal)response.data[sourceCurrency]) * (decimal)response.data[targetCurrency];
+        }
+    }
+}
---
+++ MEHR/Controllers/FoodFinderController.cs
@@ -0,0 +1,21 @@
+﻿using MEHR.Contexts;
+using MEHR.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MEHR.Controllers
+{
+    [Controller]
+    [Route("/FoodFinderApi")]
+    public class FoodFinderController:Controller
+    {
+        private DataContext _context;
+        public FoodFinderController(DataContext context) => _context = context;
+
+        [HttpGet]
+        public List<FoodLocation> GetFoodLocations(double? MaxDistanceInKM, decimal? MaxPriceInEuro, List<string> AssociatedTags, bool CurrentlyOpen, bool IsDeleviring, double locLat, double locLong)
+        {
+
+            return GenerationAlgorithms.QueryFoodFinder(new FoodFinderQuery(MaxDistanceInKM, MaxPriceInEuro, AssociatedTags, CurrentlyOpen, IsDeleviring), locLat, locLong, _context);
+        }
+    }
+}
