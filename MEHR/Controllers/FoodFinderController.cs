2155c38d8ddae0930449a4cab6f62ec8a731aec2
Cremetoertchen0815 <Mircosoft_heck@web.de>
2023-03-30 14:49:37 +0200

    Implemented splitting by delimiter into FoodFinder

--- MEHR/Controllers/FoodFinderController.cs
+++ MEHR/Controllers/FoodFinderController.cs
@@ -13,12 +13,12 @@ namespace MEHR.Controllers
         public FoodFinderController(DataContext context) => _context = context;
 
         [HttpGet]
-        public SimpleLocationInfo[] GetFoodLocations(double MaxDistanceInKM, decimal MaxPriceInEuro, List<string> AssociatedTags, bool CurrentlyOpen, bool IsDeleviring, double locLat, double locLong)
+        public SimpleLocationInfo[] GetFoodLocations(double MaxDistanceInKM, decimal MaxPriceInEuro, string AssociatedTags, bool CurrentlyOpen, bool IsDeleviring, double locLat, double locLong)
         {
             var query = GenerationAlgorithms.QueryFoodFinder(new FoodFinderQuery(MaxDistanceInKM <= 0 ? null : MaxDistanceInKM,
                                                                                  MaxPriceInEuro <= 0 ? null : MaxPriceInEuro,
-                                                                                 AssociatedTags, CurrentlyOpen, IsDeleviring),
-                                                                                 locLat, locLong, _context);
+                                                                                 (AssociatedTags ?? string.Empty).Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList(),
+                                                                                 CurrentlyOpen, IsDeleviring), locLat, locLong, _context);
             return query.Select(SimpleLocationInfo.FromFoodLocation).ToArray();
         }
     }
